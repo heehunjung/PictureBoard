6bd3d9b2c9927e6a19c463cc5e96fb8c1985fb94
heehun <slrkqk2@naver.com>
2024-03-19 14:12:02 +0900

    init

---
+++ src/main/java/com/example/Proj_Refactory/ProjRefactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proj_Refactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjRefactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjRefactoryApplication.class, args);
+	}
+
+}
